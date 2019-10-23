f048737719cf2cd1d1df5556af96cf841d57da15
adrianoenrico <adrianoenrico1@gmail.com>
2019-10-23 18:30:54 +0100

    added assets for workiing outside of koji env

--- frontend/src/scenes/Preloader.js
+++ frontend/src/scenes/Preloader.js
@@ -13,24 +13,24 @@ class Preloader extends Phaser.Scene{
         let progress = this.add.graphics();
         this.load.on('progress', (value) => {
             progress.clear();
-            progress.fillStyle(Koji.config.settings.backgroundColor, 1);
+            progress.fillStyle(Koji.config.settings.backgroundColor || "#d76b6b", 1);
             progress.fillRect(0, (this.sys.game.config.height / 2) - 30, this.sys.game.config.width * value, 60);
         });
         this.load.on('complete', () => {
             progress.destroy();
         });
         
-        this.load.image('tiles', Koji.config.assets.tileset);
+        this.load.image('tiles', Koji.config.assets.tileset || "https://images.koji-cdn.com/f852518e-69be-4b7d-9c6d-c224dfe35cd0/pbnvu-tiles.png");
         
         for(let i = 0;i < 16; i++)
         {
-            this.load.tilemapTiledJSON('map' + i, Koji.config.assets.mapArray[i]);
+            this.load.tilemapTiledJSON('map' + i, Koji.config.assets.mapArray[i] || ("./assets/map" + i + ".josn" ));
         }
 
         
-        this.load.spritesheet('player', Koji.config.settings.playerImg, {frameWidth: Koji.config.settings.playerImgSize, frameHeight: Koji.config.settings.playerImgSize});
+        this.load.spritesheet('player', Koji.config.settings.playerImg || "https://images.koji-cdn.com/f852518e-69be-4b7d-9c6d-c224dfe35cd0/vla98-player1.png", {frameWidth: Koji.config.settings.playerImgSize || 20, frameHeight: Koji.config.settings.playerImgSize || 20});
         
-        this.load.image('gamepaused', Koji.config.assets.gamepaused);
+        this.load.image('gamepaused', Koji.config.assets.gamepaused ||"https://images.koji-cdn.com/f852518e-69be-4b7d-9c6d-c224dfe35cd0/i0eh8-gamepaused.png");
 
     }
     
